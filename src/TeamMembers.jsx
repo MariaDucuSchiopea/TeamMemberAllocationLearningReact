a2322aa8f9afc89d13dc87d462bf755412446972
Maria <109592079+MariaDucuSchiopea@users.noreply.github.com>
2022-09-07 13:55:39 +0000

    Added key in TeamMember.jsx

--- src/TeamMembers.jsx
+++ src/TeamMembers.jsx
@@ -2,7 +2,7 @@ import TeamMemberCard from './TeamMemberCard'
 const TeamMembers = ({ employees, handleEmployeeCardClick, selectedTeam }) => {
   return (
     employees.map((employee) => (
-      <TeamMemberCard employee={employee} handleEmployeeCardClick={handleEmployeeCardClick} selectedTeam={selectedTeam} />
+      <TeamMemberCard key={employee.id} employee={employee} handleEmployeeCardClick={handleEmployeeCardClick} selectedTeam={selectedTeam} />
     ))
   )
 }
