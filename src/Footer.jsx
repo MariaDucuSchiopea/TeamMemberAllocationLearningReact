d433ae5a462fd39e27cecfde37a66cc19a3478f7
Maria Ducu <>
2022-09-07 11:10:53 +0000

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+
+  return(
+    <footer className="container">
+      <div className="row justify-content-center mt3- mb4">
+        <div className="col-8">
+          <h5>Team member allocation App - 2022</h5>
+        </div>
+      </div>
+    
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/NotFound.jsx
@@ -0,0 +1,15 @@
+const NotFound = () => {
+
+  return(
+    <main className="container">
+      <div className="row justify-content-center mt3- mb4">
+        <div className="col-8">
+          <h1 class="text-danger">404 - Page not found</h1>
+        </div>
+      </div>
+    
+    </main>
+  )
+}
+
+export default NotFound
